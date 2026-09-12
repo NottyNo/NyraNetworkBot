4672875a0ac2aed8669c2ac58eeb0068e4fbf39c
NottyNo <timz221108@gmx.de>
2026-09-12 19:16:44 +0200

    MOD: Fixed deployment not working | MOD: better command architecture

--- src/commands/utility/echo.js
+++ src/commands/utility/echo.js
@@ -1,23 +1,29 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { execute } = require('./ping');
 
-const data = (new SlashCommandBuilder()
-  .setName('echo')
-  .setDescription('Replies with your input!')
-  .addStringOption((option) =>
-    option
-      .setName('input')
-      .setDescription('The input to echo back')
-      .setRequired(true),
-  )
-  .addBooleanOption((option) =>
-    option
-      .setName('ephemeral')
-      .setDescription('Wether or not the reply should be ephemeral')
-      .setRequired(false),
-  ).execute = async (interaction) => {
-  const input = interaction.options.getString('input');
-  const ephemeral = interaction.options.getBoolean('ephemeral') || false;
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption((option) =>
+      option
+        .setName('input')
+        .setDescription('The input to echo back')
+        .setRequired(true),
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName('ephemeral')
+        .setDescription('Whether or not the reply should be ephemeral')
+        .setRequired(false),
+    ),
 
-  interaction.reply({ content: input, ephemeral: ephemeral });
-});
+  async execute(interaction) {
+    const input = interaction.options.getString('input');
+    const ephemeral = interaction.options.getBoolean('ephemeral') || false;
+
+    await interaction.reply({
+      content: input,
+      flags: ephemeral ? ephemeral : undefined,
+    });
+  },
+};
